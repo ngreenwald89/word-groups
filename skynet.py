defbd7fd1c88856a3906cfbb6a9987450e8acd0e
Nat Greenwald <nat.greenwald@gmail.com>
2017-01-17 10:09:24 -0500

    trie

--- skynet.py
+++ skynet.py
@@ -1,6 +1,8 @@
+import re
 import nltk
 nltk.data.path.append('./nltk_data/')
 from nltk.corpus import wordnet as wn
+import marisa_trie
 
 # http://stackoverflow.com/questions/26222484/determining-hypernym-or-hyponym-using-wordnet-nltk
 
@@ -8,5 +10,22 @@ def hypodomain(domain):
 	domain = wn.synsets(domain, pos='n')[0]
 	return set([i for i in domain.closure(lambda s:s.hyponyms())])
 
+def first_n(word): 
+	return wn.synset('{}.n.01'.format(word))
+
+
+find=re.compile(r"[\D]+?[?=.]")
+
+def the_word(syn):
+	return re.search(find, syn.name()).group(0)[:-1]
+
+def word_list(hyponyms):
+	hypolist = []
+	for h in hyponyms:
+		hypolist.append(the_word(h))
+	return hypolist
+
 if __name__ == '__main__':
 	hypotools = hypodomain('tool')
+	tools = word_list(hypotools)
+	toolTrie = marisa_trie.Trie(tools)
