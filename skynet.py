fab1fda1db7ef118592a9e01353f62e58b564b26
Nat Greenwald <nat.greenwald@gmail.com>
2017-01-16 23:46:51 -0500

    hyponyms

--- skynet.py
+++ skynet.py
@@ -1,3 +1,12 @@
 import nltk
 nltk.data.path.append('./nltk_data/')
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet as wn
+
+# http://stackoverflow.com/questions/26222484/determining-hypernym-or-hyponym-using-wordnet-nltk
+
+def hypodomain(domain):
+	domain = wn.synsets(domain, pos='n')[0]
+	return set([i for i in domain.closure(lambda s:s.hyponyms())])
+
+if __name__ == '__main__':
+	hypotools = hypodomain('tool')
